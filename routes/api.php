5df34e9b5e0cc769fc9cc959ab58c074623da2fe
th-drakopoulos <th.drakopoulos@tms.com.gr>
2022-02-09 23:11:49 +0200

    feat: add web and api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +13,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'movies', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [MovieController::class, 'index']);
+    Route::post('add', [BookController::class, 'add']);
 });
