055e25cb950b366d48f34d5e1a388ed2a2126996
th-drakopoulos <th.drakopoulos@tms.com.gr>
2022-02-10 19:10:28 +0200

    feat: add momentjs

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,66 +1,75 @@
 <template>
-  <div class="container mt-2 bg-white">
-    <nav class="navbar navbar-blue border-bottom">
-      <!-- for logged-in user-->
-      <div class="container-fluid" v-if="isLoggedIn">
-        <router-link to="/" class="navbar-brand">MovieRama</router-link>
-        <div class="d-flex">
-          <router-link to="/movies" class="nav-item nav-link"
-            >Movies</router-link
-          >
-          <a class="nav-item nav-link" style="cursor: pointer" @click="logout"
-            >Logout</a
-          >
-        </div>
-      </div>
-      <!-- for non-logged user-->
-      <div class="container-fluid" v-else>
-        <router-link to="/" class="nav-item nav-link">MovieRama</router-link>
-        <div class="d-flex">
-          <router-link to="/login" class="nav-item nav-link">Login</router-link>
-          <router-link to="/register" class="nav-item nav-link"
-            >Register
-          </router-link>
-        </div>
-      </div>
-    </nav>
-    <br />
-    <router-view />
-  </div>
+    <div class="container mt-2 bg-white">
+        <nav class="navbar navbar-blue border-bottom">
+            <!-- for logged-in user-->
+            <div class="container-fluid" v-if="isLoggedIn">
+                <router-link to="/" class="navbar-brand h1"
+                    >MovieRama</router-link
+                >
+                <div class="d-flex">
+                    <router-link to="/movies" class="nav-item nav-link"
+                        >Movies</router-link
+                    >
+                    <a
+                        class="nav-item nav-link"
+                        style="cursor: pointer"
+                        @click="logout"
+                        >Logout</a
+                    >
+                </div>
+            </div>
+            <!-- for non-logged user-->
+            <div class="container-fluid" v-else>
+                <router-link to="/" class="nav-item nav-link h1"
+                    >MovieRama</router-link
+                >
+                <div class="d-flex">
+                    <router-link to="/login" class="nav-item nav-link"
+                        >Login</router-link
+                    >
+                    <router-link to="/register" class="nav-item nav-link"
+                        >Register
+                    </router-link>
+                </div>
+            </div>
+        </nav>
+        <br />
+        <router-view />
+    </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      isLoggedIn: false,
-    };
-  },
-  created() {
-    if (window.Laravel.isLoggedin) {
-      this.isLoggedIn = true;
-    }
-  },
-  methods: {
-    logout(e) {
-      console.log("ss");
-      e.preventDefault();
-      this.$axios.get("/sanctum/csrf-cookie").then((response) => {
-        this.$axios
-          .post("/api/logout")
-          .then((response) => {
-            if (response.data.success) {
-              window.location.href = "/";
-            } else {
-              console.log(response);
-            }
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      });
+    name: "App",
+    data() {
+        return {
+            isLoggedIn: false,
+        };
     },
-  },
+    created() {
+        if (window.Laravel.isLoggedin) {
+            this.isLoggedIn = true;
+        }
+    },
+    methods: {
+        logout(e) {
+            console.log("ss");
+            e.preventDefault();
+            this.$axios.get("/sanctum/csrf-cookie").then((response) => {
+                this.$axios
+                    .post("/api/logout")
+                    .then((response) => {
+                        if (response.data.success) {
+                            window.location.href = "/";
+                        } else {
+                            console.log(response);
+                        }
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            });
+        },
+    },
 };
 </script>
--- resources/js/components/Movies.vue
+++ resources/js/components/Movies.vue
@@ -1,64 +1,76 @@
 <template>
-  <div>
-    <div
-      class="card mb-3"
-      v-for="(movie, index) in movies"
-      :key="'movie-' + index"
-    >
-      <div class="card-body">
-          <h3 class="card-title">
-            {{ movie.title }}
-          </h3>
-          <div class="card-text mb-2">{{ movie.user_name }}</div>
-          <div class="card-text mb-2">{{ movie.description }}</div>
-          <div class="card-text">{{ movie.likes }} likes | {{ movie.hates }} hates</div>
+    <div>
+        <div
+            class="card mb-3"
+            v-for="(movie, index) in movies"
+            :key="'movie-' + index"
+        >
+            <div class="card-body">
+                <h3 class="card-title">
+                    {{ movie.title }}
+                </h3>
+                <div class="card-text mb-2">
+                    Posted by {{ movie.user_name }}
+                    {{ fromNow(movie.created_at) }}
+                </div>
+                <div class="card-text mb-2">{{ movie.description }}</div>
+                <div
+                    class="card-text"
+                    v-if="movie.likes > 0 || movie.hates > 0"
+                >
+                    {{ movie.likes }} likes | {{ movie.hates }} hates
+                </div>
+            </div>
         </div>
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
 import axios from "axios";
+import moment from "moment";
 export default {
-  data() {
-    return {
-      loading: false,
-      movies: null,
-    };
-  },
-  async created() {
-    try {
-      this.loading = true;
-      const response = (await axios.get("/api/movies")).data;
-      if (response) {
-        this.movies = response.data;
-      }
-    } catch (error) {
-      console.warn(error);
-    } finally {
-      this.loading = false;
-    }
-  },
-  methods: {
-    deleteBook(id) {
-      this.$axios.get("/sanctum/csrf-cookie").then((response) => {
-        this.$axios
-          .delete(`/api/books/delete/${id}`)
-          .then((response) => {
-            let i = this.books.map((item) => item.id).indexOf(id); // find index of your object
-            this.books.splice(i, 1);
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      });
+    data() {
+        return {
+            loading: false,
+            movies: null,
+        };
     },
-  },
-  beforeRouteEnter(to, from, next) {
-    if (!window.Laravel.isLoggedin) {
-      window.location.href = "/";
-    }
-    next();
-  },
+    async created() {
+        try {
+            this.loading = true;
+            const response = (await axios.get("/api/movies")).data;
+            if (response) {
+                this.movies = response.data;
+            }
+        } catch (error) {
+            console.warn(error);
+        } finally {
+            this.loading = false;
+        }
+    },
+    methods: {
+        deleteBook(id) {
+            this.$axios.get("/sanctum/csrf-cookie").then((response) => {
+                this.$axios
+                    .delete(`/api/books/delete/${id}`)
+                    .then((response) => {
+                        let i = this.books.map((item) => item.id).indexOf(id); // find index of your object
+                        this.books.splice(i, 1);
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            });
+        },
+        fromNow(value) {
+            return moment(value).fromNow();
+        },
+    },
+    beforeRouteEnter(to, from, next) {
+        if (!window.Laravel.isLoggedin) {
+            window.location.href = "/";
+        }
+        next();
+    },
 };
 </script>
